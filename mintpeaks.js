f9d99b017619bc8de43c1e11fce310e6cddf2a2c
Miha Rebernik <miha@rebernik.info>
2015-07-19 21:47:29 -0700

    Tidying up the server file and adding debug library.

--- mintpeaks.js
+++ mintpeaks.js
@@ -1,29 +1,53 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var net = require('net');
+var debug = require('debug')('mintpeaks:server');
 
-var HOST = '0.0.0.0';
-var PORT = 10231;
+// Tessel TCP server settings.
+var TESSEL_SERVER_HOST = '0.0.0.0';
+var TESSEL_SERVER_PORT = 10231;
+
+// HTTP server settings.
+var HTTP_SERVER_PORT = 3000;
+
+// Socket.io connection logging.
+io.on('connection', function(sckt) {
+  debug('Browser connected');
+
+   sckt.on('disconnect', function() {
+    debug('Browser disconnected');
+  });
+});
+
+// Start up HTTP server.
+http.listen(HTTP_SERVER_PORT, function() {
+  debug('HTTP server listening on *:' + HTTP_SERVER_PORT);
+});
 
 // Callback runs for each created connection.
 net.createServer(function(sckt) {
   // A connection was initiated.
-  console.log('Connection', sckt.remoteAddress, sckt.remotePort);
+  debug('Tessel connected', sckt.remoteAddress, sckt.remotePort);
 
   sckt.on('data', function(data) {
     data = data.toString('utf8');
 
     try {
       var parsed_data = JSON.parse(data);
-      console.log('Received data', parsed_data);
+      debug('Received data from Tessel', parsed_data);
 
+      // Emit received data through Socket.io.
+      io.emit('received_data', parsed_data);
     } catch (e) {
-      console.log('ERROR: Received non JSON data', data);
+      debug('ERROR: Received non JSON data from Tessel', data);
     }
   });
 
   // Connection closed handler.
   sckt.on('close', function() {
-    console.log('Disconnected', sckt.remoteAddress, sckt.remotePort);
+    debug('Tessel disconnected');
   });
-}).listen(PORT, HOST);
+}).listen(TESSEL_SERVER_PORT, TESSEL_SERVER_HOST);
 
-console.log('Server listening on ' + HOST + ':' + PORT);
+debug('Tessel server listening on ' + TESSEL_SERVER_HOST + ':' + TESSEL_SERVER_PORT);
